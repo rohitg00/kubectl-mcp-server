60becef2ba3b1ed7c2eaa7e1432c81ea185e93e9
Rohit Ghumare <ghumare64@gmail.com>
2026-01-25 01:50:14 +0000

    chore: Bump version to 1.17.0

--- kubectl_mcp_tool/__init__.py
+++ kubectl_mcp_tool/__init__.py
@@ -7,7 +7,7 @@ with Kubernetes clusters through natural language commands.
 For more information, see: https://github.com/rohitg00/kubectl-mcp-server
 """
 
-__version__ = "1.19.0"
+__version__ = "1.17.0"
 
 from .mcp_server import MCPServer
 from .diagnostics import run_diagnostics, check_kubectl_installation, check_cluster_connection
